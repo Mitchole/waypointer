ac3fb358d835a75acb84e32bcea3564c6845a4b4
Mitchole <mitchole4@gmail.com>
2026-05-25 16:20:41 +0100

    PresetBrowserPanelTest: drop unused assertFalse import

--- src/test/java/com/waypointer/ui/PresetBrowserPanelTest.java
+++ src/test/java/com/waypointer/ui/PresetBrowserPanelTest.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 import net.runelite.client.game.SpriteManager;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
